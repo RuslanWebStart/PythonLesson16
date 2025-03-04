7501119eaf75da67f5d774517d07bb34f80e109e
RuslanWebStart <idrufa@yandex.ru>
2025-03-04 13:15:41 +0300

    изменение задания 1 и 2

--- PythonLesson16/PythonLesson16.py
+++ PythonLesson16/PythonLesson16.py
@@ -14,7 +14,7 @@ class CashRegister:
         print('В кассу добавленно:',x)
 
     def count_1000(self):
-        print('В кассе всего:',int(self.money))
+        print('Количество целых тысяч в кассе:',int(self.money // 1000))
 
     def take_away(self,x):
         if x <= self.money:
@@ -26,9 +26,9 @@ class CashRegister:
 
 
 cashRegister1 = CashRegister(1000)
-cashRegister1.top_up(100)
+cashRegister1.top_up(1000)
 cashRegister1.count_1000()
-cashRegister1.take_away(300)
+cashRegister1.take_away(400)
 
 # Задача 2
 
@@ -67,24 +67,19 @@ class Turtle:
             print('s не может быть меньше или равно 0')
 
     def count_moves(self, x2, y2):
+        dx = abs(x2 - self.x)
+        dy = abs(y2 - self.y)
 
-        dx = x2 - self.x
-        dy = y2 - self.y
+        moves_x = (dx + self.s - 1) // self.s
+        moves_y = (dy + self.s - 1) // self.s
 
-        steps_x = (dx / self.s) + (1 if dx % self.s != 0 and dx != 0 else 0)
-        steps_y = (dy / self.s) + (1 if dy % self.s != 0 and dy != 0 else 0)
-
-        steps_x = int(steps_x) + (1 if steps_x > int(steps_x) else 0)
-        steps_y = int(steps_y) + (1 if steps_y > int(steps_y) else 0)
-
-        if steps_x > steps_y:
-            return steps_x
-        else:
-           return steps_y
+        return max(moves_x, moves_y)
           
 
 
 turtle = Turtle()
 turtle.go_up()
 turtle.go_right()
-print('Вывод минимального количества действий до позиции',turtle.count_moves(5, 5))
+turtle.evolve()
+moves = turtle.count_moves(5, 5)
+print('Минимальное количество движений:', moves)
